5d6bb44951cc61b4e1abf4b9b1d66fe4d94cd4c4
Mac Rowe <m.rowe42@gmail.com>
2021-02-26 14:30:22 -0500

    commit tree 9764551b744483171e743228156fd431f6793a86

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,15 +1,20 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+def my_hash()
+  mac_hash = {
+    "my_name" => "Mac", 
+    "my_age" => 31
+  }
 end
 
 
-def shipping_manifest
+def shipping_manifest()
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3,
+  }
 
 end
 
@@ -22,6 +27,8 @@ def retrieval
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
+  shipping_manifest["oil paintings"]
+
 end
 
 def adding
@@ -32,12 +39,14 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+                 shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+                 shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
+
+                 shipping_manifest
 
 end
